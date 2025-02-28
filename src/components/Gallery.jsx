4b2bdd8f1f534b221c7b496dd40518abb6029519
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-28 16:03:24 +0000

    add gallery update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,8 @@
 "use client";
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import LightGallery from "lightgallery/react";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import Link from "next/link";
 import Image from "next/image";
 import {
   MdOutlineArrowForwardIos,
@@ -17,17 +20,61 @@ import "swiper/css/pagination";
 import { FreeMode, Pagination, Navigation } from "swiper/modules";
 
 const images = [
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/casa_8_crsxvk",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/yntqxtyrr6twlbnk7fma",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/j29rwwjluxdhjs1sdufo",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/aeuldjydltg3zgisujem",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/akkjaul1qfiskaqrg507",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/x7ugtlvtbdmnttlimlpg",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/pmztmaraj71g4g4xicbe",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/ygvykfxjvir2dtkfdiie",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/pjdoqvpfbeuldq96mewg",
-  "https://res.cloudinary.com/dz7wroord/image/upload/c_crop,g_south,h_4240,w_4160/casalalla-v2/nczsxh4f2gndgy2h6uqt",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/casa_11_lkfatp",
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/casa_8_crsxvk",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/casa_8_crsxvk",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/yntqxtyrr6twlbnk7fma",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/yntqxtyrr6twlbnk7fma",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/j29rwwjluxdhjs1sdufo",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/j29rwwjluxdhjs1sdufo",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/aeuldjydltg3zgisujem",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/aeuldjydltg3zgisujem",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/akkjaul1qfiskaqrg507",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/akkjaul1qfiskaqrg507",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/x7ugtlvtbdmnttlimlpg",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/x7ugtlvtbdmnttlimlpg",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/pmztmaraj71g4g4xicbe",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/pmztmaraj71g4g4xicbe",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/ygvykfxjvir2dtkfdiie",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/ygvykfxjvir2dtkfdiie",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/pjdoqvpfbeuldq96mewg",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/pjdoqvpfbeuldq96mewg",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/c_crop,g_south,h_4240,w_4160/casalalla-v2/nczsxh4f2gndgy2h6uqt",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/c_crop,g_south,h_4240,w_4160/casalalla-v2/nczsxh4f2gndgy2h6uqt",
+  },
+  {
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/casa_11_lkfatp",
+    thumbnail:
+      "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/casa_11_lkfatp",
+  },
 ];
 
 const Gallery = () => {
@@ -61,7 +108,11 @@ const Gallery = () => {
         className="relative w-full md:min-h-[75vh] min-h-[55vh] bg-[url('https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/casalalla-v2/p0tmas5drbxexrnxffzo')] bg-cover bg-center bg-no-repeat"
       >
         <div className="absolute inset-0 bg-black opacity-60"></div>
-        <div className="absolute w-full h-full flex justify-center items-center px-9 md:px-12 lg:px-32">
+        <LightGallery
+          speed={500}
+          plugins={[lgThumbnail]}
+          elementClassNames="absolute w-full h-full flex justify-center items-center px-9 md:px-12 lg:px-32"
+        >
           <Swiper
             slidesPerView={
               isMobile === "mobile" ? 1 : isMobile === "tab" ? 2 : 3
@@ -73,19 +124,24 @@ const Gallery = () => {
               nextEl: ".swiper-button-next-ex1",
               prevEl: ".swiper-button-prev-ex1",
             }}
-            className="mySwiper"
+            className=""
           >
             {images.map((image, index) => (
               <SwiperSlide key={index}>
-                <Image
-                  src={image}
-                  height={800}
-                  width={800}
-                  className="object-cover w-80 h-80 md:w-[22rem] md:h-[22rem] lg:w-[29rem] lg:h-[29rem] rounded-md"
-                />
+                <Link href={image.thumbnail}>
+                  <Image
+                    src={image.src}
+                    width={800}
+                    height={800}
+                    loading="lazy"
+                    className="object-cover w-80 h-80 md:w-[22rem] md:h-[22rem] lg:w-[29rem] lg:h-[29rem] rounded-md"
+                    alt={`Casa lalla takerkoust ${index}`}
+                  />
+                </Link>
               </SwiperSlide>
             ))}
           </Swiper>
+
           {isMobile === "desktop" && (
             <>
               <button className="swiper-button-prev-ex1 grid place-content-center left-16 transition text-white absolute z-[999] top-1/2 -translate-y-1/2">
@@ -96,7 +152,7 @@ const Gallery = () => {
               </button>
             </>
           )}
-        </div>
+        </LightGallery>
       </div>
     )
   );
