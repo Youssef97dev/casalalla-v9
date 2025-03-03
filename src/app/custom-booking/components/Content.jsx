ec63a69b57c6482a18fe26803b4f98d902cffc2c
Youssef97dev <youssefmoussafi97@gmail.com>
2025-03-03 10:35:25 +0000

    fix error

--- src/app/custom-booking/components/Content.jsx
+++ src/app/custom-booking/components/Content.jsx
@@ -14,9 +14,9 @@ const Content = () => {
       <div className="w-full mt-36 lg:mt-32 flex flex-col justify-center items-center gap-3 px-5 lg:px-80 text-center text-bg_intro">
         <h1 className="text-[30.57px] leading-[33.22px]">Contactez-nous</h1>
         <p className="text-[16px] leading-[22.5px]">
-          Pour les rassemblements, les célébrations spéciales, les séances
+          {`Pour les rassemblements, les célébrations spéciales, les séances
           photos ou pour plus d'informations sur nous, veuillez nous envoyer un
-          message en utilisant le formulaire de contact ci-dessous.
+          message en utilisant le formulaire de contact ci-dessous.`}
         </p>
         <div className="text-[16px] leading-[22.5px] flex justify-center items-center gap-2">
           <span>Pour nous envoyer un courriel : </span>
